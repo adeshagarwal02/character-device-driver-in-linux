c5db90b1ef66845625270d67327d8647f08a5684
Adesh Agarwal <adeshagarwal02@gmail.com>
2017-12-05 08:37:49 +0530

    registering device and making scull

--- scull_register/modules/.Adesh.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/adesh/fight/review/ch_driver/scull_register/Adesh.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/adesh/fight/review/ch_driver/scull_register/Adesh.ko /home/adesh/fight/review/ch_driver/scull_register/Adesh.o /home/adesh/fight/review/ch_driver/scull_register/Adesh.mod.o ;  true
--- scull_register/modules/.Adesh.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/adesh/fight/review/ch_driver/scull_register/Adesh.o := ld -m elf_x86_64   -r -o /home/adesh/fight/review/ch_driver/scull_register/Adesh.o /home/adesh/fight/review/ch_driver/scull_register/init.o /home/adesh/fight/review/ch_driver/scull_register/clean.o 
